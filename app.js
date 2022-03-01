4845e285acab548a47e5daaea24b5ea739ec0bff
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-03-01 13:13:31 +0600

    details added

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ const phoneDetails = (phones) => {
         phoneDiv.classList.add('mb-3')
 
         phoneDiv.innerHTML = `
-        <div class="card" >
+        <div onclick="phoneDetail('${phone.slug}')"  class="card" >
         <img class="card-img-top" src="${phone.image}" >
         <div class="card-body">
           <h4 class="card-title">Brand:${phone.brand}</h4>
           <p class="card-text">Name:${phone.phone_name}  </p>
-          <button onclick="phoneDetail('${phone.slug}')"  class="btn btn-primary">Details</button>
+          <button type="button"  class="btn btn-primary">Details</button>
         </div>
       </div>
         `
@@ -39,12 +39,19 @@ const phoneDetail = (id) => {
         .then(data => showPhoneDetail(data.data))
 }
 
-const showPhoneDetail = (phone) => {
+const showPhoneDetail = (detail) => {
 
-    c
+    console.log(detail)
     const details = document.getElementById('more-details')
     const detailDiv = document.createElement('div');
-    detailDiv.innerHTML = ` <div> <img src="${phone.image}>    <h5> ${phone.name}  </h5>  <p> Release Date: ${phone.releaseDate} </p> <p> Main Features: Chipset ${phone.mainFeatures.chipset}  </p> <p> Memory: ${phone.mainFeatures.memory}  </p> <p> Display: ${phone.mainFeatures.display} </p> <p> ID:${phone.slug} </p>    </div>`
+    detailDiv.classList.add("card")
+    detailDiv.innerHTML = `<div>  <img class="card-img-top" src="${detail.image}" >
+    <div class="card-body">
+    <p>Brand: ${detail.brand} </p>
+    <p> Release Date: ${detail.releaseDate} </p> <p> Main Features: Chipset ${detail.mainFeatures.chipset}  </p> <p> Memory: ${detail.mainFeatures.memory}  </p> <p> Display: ${detail.mainFeatures.display} </p> <p> ID:${detail.slug} </p> </div>
+      
+    </div>`
+
 
     details.appendChild(detailDiv)
 
