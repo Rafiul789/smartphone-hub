c295556e5948aef12a6fb781c8c9a67b1a8f5131
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-03-01 14:20:37 +0600

    spinner added

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
-// getting phone searchbar
+// loading spinner
+const spinner = displaySpinner => {
+    document.getElementById('spinner').style.display = displaySpinner
+}
 
+// getting phone searchbar
 const searchPhone = () => {
+
         const phone = document.getElementById('search-phone').value
+            // spinner
+        spinner('block')
+            // loading phone data
         const url = `https://openapi.programming-hero.com/api/phones?search=${phone}`
         fetch(url)
             .then((response) => response.json())
-            .then(data => phoneDetails(data.data))
+            .then(data => phoneDetails(data.data.slice(0, 20)))
+
     }
     // showing phone information
 const phoneDetails = (phones) => {
-
+        const showPhone = document.getElementById('phones-container')
+        showPhone.textContent = ''
 
         for (const phone of phones) {
-            const showPhone = document.getElementById('phones-container')
+
             const phoneDiv = document.createElement('div')
             phoneDiv.classList.add('col-md-4')
             phoneDiv.classList.add('mb-3')
 
             phoneDiv.innerHTML = `
-        <div onclick="phoneDetail('${phone.slug}')"  class="card" >
+        <div   class="card" >
         <img class="card-img-top" src="${phone.image}" >
         <div class="card-body">
           <h4 class="card-title">Brand:${phone.brand}</h4>
           <p class="card-text">Name:${phone.phone_name}  </p>
-          <button type="button"  class="btn btn-primary">Details</button>
+          <button onclick="phoneDetail('${phone.slug}')"   type="button"  class="btn btn-primary">Details</button>
         </div>
       </div>
         `
             showPhone.appendChild(phoneDiv)
-        }
 
+        }
+        spinner('none')
     }
     // getting the specification of phone
 const phoneDetail = (id) => {
@@ -45,12 +56,13 @@ const showPhoneDetail = (detail) => {
 
     console.log(detail)
     const details = document.getElementById('more-details')
+    details.textContent = ''
     const detailDiv = document.createElement('div');
     detailDiv.classList.add("card")
     detailDiv.innerHTML = `<div>  <img class="card-img-top" src="${detail.image}" >
     <div class="card-body">
     <p>Brand: ${detail.brand} </p>
-    <p> Release Date: ${detail.releaseDate} </p> <p> Main Features: Chipset ${detail.mainFeatures.chipset}  </p> <p> Memory: ${detail.mainFeatures.memory}  </p> <p> Display: ${detail.mainFeatures.display} </p> <p> ID:${detail.slug} </p> </div>
+    <p> Release Date: ${detail.releaseDate} </p> <p> Main Features: Chipset ${detail.mainFeatures.chipSet}  </p> <p> Memory: ${detail.mainFeatures.memory}  </p> <p> Display: ${detail.mainFeatures.displaySize} </p> <p> ID:${detail.slug} </p> <p>Storage:${detail.mainFeatures.storage}</div>
       
     </div>`
 
